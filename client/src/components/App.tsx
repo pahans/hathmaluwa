c79526cc8a22a15cf248125026e6b169938841dd
pahans <pahan@wso2.com>
2020-01-11 01:48:51 +0530

    Add meterial ui

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,10 +1,36 @@
 import * as React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Divider from '@material-ui/core/Divider';
 
-export interface HelloProps { compiler: string; framework: string; }
+import Header from './Header';
+import Footer from './Footer';
 
-export default class Welcome extends React.Component {
-    render() {
-      return <h1>Pretty Soon There Will Be an Awesome React App Deployed here.</h1>;
-    }
+const sections = [
+  { title: 'Technology', url: '#' },
+  { title: 'Design', url: '#' },
+  { title: 'Culture', url: '#' },
+  { title: 'Business', url: '#' },
+  { title: 'Politics', url: '#' },
+  { title: 'Opinion', url: '#' },
+  { title: 'Science', url: '#' },
+  { title: 'Health', url: '#' },
+  { title: 'Style', url: '#' },
+  { title: 'Travel', url: '#' },
+];
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Header title="Hathmaluwa" sections={sections} />
+        <main>
+         
+        </main>
+      </Container>
+      <Divider />
+      <Footer title="Footer" description="Something here to give the footer a purpose!" />
+    </React.Fragment>
+  );
 }
-
