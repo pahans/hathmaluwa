fdecb889416442ff43277f987f36fcebb053d287
pahans <pahan@wso2.com>
2020-03-24 01:20:02 +0530

    Add react-app-polyfill

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,6 +2,8 @@ import 'typeface-roboto';
 import App from './components/Home';
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 
 ReactDOM.hydrate(
     <App />,
