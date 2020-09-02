bf1dcaa57ff51a60a06553fd65ca338e9af50b46
pahans <pahan@wso2.com>
2020-09-03 03:20:51 +0530

    Add awesome typescript loader

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,8 +2,6 @@ import 'typeface-roboto';
 import App from './components/App';
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.hydrate(
