b6b2ff792c34426c5477c156d0d4c95cff7a0371
Pahan Sarathchandra <pahan123@gmail.com>
2021-09-05 20:49:38 +0530

    Change page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Hathmaluwa</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
