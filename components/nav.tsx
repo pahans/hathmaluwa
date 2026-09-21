c0599b0828045d5924f43c803c114ff1e1b6bcbe
Pahan Sarathchandra <pahan123@gmail.com>
2026-09-21 09:49:40 +0300

    Fix subscribe url

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,20 +1,20 @@
-'use client'
+'use client';
 
-import Link from 'next/link'
-import { usePathname, useSearchParams } from 'next/navigation'
-import ThemeToggle from './theme-toggle'
+import Link from 'next/link';
+import { usePathname, useSearchParams } from 'next/navigation';
+import ThemeToggle from './theme-toggle';
 
 const LINKS = [
   { href: '/', label: 'Home' },
-  { href: '/subscribe', label: 'Subscribe' },
+  { href: 'https://feeds.feedburner.com/hathmaluwa/all', label: 'Subscribe' },
   { href: '/badge', label: 'Badge' },
   { href: '/contact', label: 'Contact' },
-]
+];
 
 function NavBar() {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-  const query = searchParams.get('q') ?? ''
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const query = searchParams.get('q') ?? '';
 
   return (
     <header className="hm-site-header">
@@ -22,12 +22,21 @@ function NavBar() {
         <Link className="hm-brand" href="/" aria-label="Hathmaluwa home">
           {/* The logo is used exactly as supplied. See design/hathmaluwa/logos-README.md. */}
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/badges/hathmaluwa-horizontal.png" width={131} height={40} alt="හත්මාළුව Hathmaluwa" />
+          <img
+            src="/badges/hathmaluwa-horizontal.png"
+            width={131}
+            height={40}
+            alt="හත්මාළුව Hathmaluwa"
+          />
         </Link>
 
         <nav className="hm-nav" aria-label="Main">
           {LINKS.map((link) => (
-            <Link key={link.href} href={link.href} aria-current={pathname === link.href ? 'page' : undefined}>
+            <Link
+              key={link.href}
+              href={link.href}
+              aria-current={pathname === link.href ? 'page' : undefined}
+            >
               {link.label}
             </Link>
           ))}
@@ -37,7 +46,15 @@ function NavBar() {
           <label htmlFor="hm-q" className="hm-sr-only">
             Search posts
           </label>
-          <input id="hm-q" name="q" type="search" placeholder="Search" autoComplete="off" defaultValue={query} key={query} />
+          <input
+            id="hm-q"
+            name="q"
+            type="search"
+            placeholder="Search"
+            autoComplete="off"
+            defaultValue={query}
+            key={query}
+          />
         </form>
 
         <ThemeToggle />
@@ -46,7 +63,7 @@ function NavBar() {
         </Link>
       </div>
     </header>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
